2ef55bf7d1d82423a2379f5ab3b017ac93eefa66
Liam Hall <hello@wearethreebears.co.uk>
2021-03-10 13:22:49 +0000

    v.0.10.0

---
+++ plugins/jumpstart.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Jumpstart from '@wearethreebears/jumpstart/index.cjs'
+
+Vue.component('FormWrapper', Jumpstart.FormWrapper)
+Vue.component('FormGroup', Jumpstart.FormGroup)
+Vue.component('FormInput', Jumpstart.FormInput)
+
+Vue.component('JumpstartButton', Jumpstart.JumpstartButton)
+
+Vue.component('JumpstartImage', Jumpstart.JumpstartImage)
+
+Vue.component('Grid', Jumpstart.Grid)
+Vue.component('GridSection', Jumpstart.GridSection)
+Vue.component('GridSectionSpan', Jumpstart.GridSectionSpan)
+Vue.component('Spacer', Jumpstart.Spacer)
+
+Vue.component('Tabs', Jumpstart.Tabs)
+
+Vue.component('TextBody', Jumpstart.TextBody)
+Vue.component('TextTitle', Jumpstart.TextTitle)
+
